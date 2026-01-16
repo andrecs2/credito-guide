0a77935179dd44e388555b9a81c720c085bf5c12
André Carlos <andrecs2@gmail.com>
2026-01-16 14:23:54 -0300

    Criação do swagger para documentação

--- backend/src/main/java/com/andrecs2/credito_guide/adapter/in/web/CreditoControllerAdapter.java
+++ backend/src/main/java/com/andrecs2/credito_guide/adapter/in/web/CreditoControllerAdapter.java
@@ -1,9 +1,7 @@
 package com.andrecs2.credito_guide.adapter.in.web;
 
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
-import java.util.stream.Collectors;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
@@ -12,26 +10,37 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.tags.Tag;
+
 @RestController
 @RequestMapping("/api/creditos")
 @CrossOrigin(origins = "*")
-public class CreditoControllerAdapter {
+@Tag(name = "API de Consulta de Créditos")
 
+public class CreditoControllerAdapter {
 
     String retorno = "primeiro_teste";
 
-    @GetMapping(value="/{numeroNfse}",  produces = "application/json")
+    @GetMapping(value = "/{numeroNfse}", produces = "application/json")
+    @Operation(summary = "Consultar créditos por número da NFS-e")
+
     public ResponseEntity<List<String>> consultarPorNfse(
+            @Parameter(description = "Número da NFS-e", required = true, example = "7891011")
+
             @PathVariable String numeroNfse) {
 
-        List<String> teste = Arrays.asList(new String[] {  retorno });
+        List<String> teste = Arrays.asList(new String[] { retorno });
 
         return ResponseEntity.ok(teste);
     }
 
-    @GetMapping(value="/credito/{numeroCredito}",     produces = "application/json" )
+    @GetMapping(value = "/credito/{numeroCredito}", produces = "application/json")
+    @Operation(summary = "Consultar crédito por número do crédito")
+
     public ResponseEntity<String> consultarPorNumeroCredito(
-            @PathVariable String numeroCredito) {
+            @Parameter(description = "Número do crédito", required = true) @PathVariable String numeroCredito) {
 
         return ResponseEntity.ok(retorno);
     }
